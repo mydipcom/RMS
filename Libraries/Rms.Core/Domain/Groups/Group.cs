416362e71f5d75c6e957ef957d9681f775ea4e8e
andy.lin <andy.lin@campray.com>
2014-11-27 18:27:18 +0800

    Add group

---
+++ Libraries/Rms.Core/Domain/Groups/Group.cs
@@ -0,0 +1,55 @@
+﻿using Rms.Core.Domain.Localization;
+using Rms.Core.Domain.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rms.Core.Domain.Groups
+{
+    public partial class Group : BaseEntity, ILocalizedEntity
+    {
+        public int TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        public string Description { get; set; } 
+
+        public int ParentId { get; set; }
+
+        public int CreatedCustomerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance update
+        /// </summary>
+        public DateTime UpdatedOnUtc { get; set; }
+ 
+    }
+}
