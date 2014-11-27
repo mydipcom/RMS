c9bad7997051673433b529ade8f867f60cd44d56
andy.lin <andy.lin@campray.com>
2014-11-27 19:02:22 +0800

    Add tenant mapping

---
+++ Libraries/Rms.Data/Mapping/Tenants/Tenant.cs
@@ -0,0 +1,19 @@
+﻿using Rms.Core.Domain.Tenants;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rms.Data.Mapping.Tenants
+{
+    public partial class TenantMap : EntityTypeConfiguration<Tenant>
+    {
+        public TenantMap()
+        {
+            this.ToTable("Tenant");
+            this.HasKey(a => a.Id);
+        }
+    }
+}
