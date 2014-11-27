42d7a0dfe7da20fcfb6d1e85e914eb27acc43523
andy.lin <andy.lin@campray.com>
2014-11-27 18:52:12 +0800

    Add group table mapping

---
+++ Libraries/Rms.Data/Mapping/Groups/GroupMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rms.Core.Domain.Groups;
+
+namespace Rms.Data.Mapping.Groups
+{
+    public partial class GroupMap : EntityTypeConfiguration<Group>
+    {
+        public GroupMap()
+        {
+            this.ToTable("Group");
+            this.HasKey(a => a.Id);
+
+        }
+    }
+}
