0d8abc8f0c5418a5269e95661f5177be4b062a94
andy.lin <andy.lin@campray.com>
2014-12-09 15:45:34 +0800

    Add industry domain

---
+++ Libraries/Rms.Core/Domain/Common/Industry.cs
@@ -0,0 +1,22 @@
+﻿using Rms.Core.Domain.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rms.Core.Domain.Common
+{
+    public partial class Industry : BaseEntity, ILocalizedEntity
+    {
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+}
