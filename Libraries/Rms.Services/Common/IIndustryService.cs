e29b8e470ea84b7e196f0cff834e2e9f8fa369c9
andy.lin <andy.lin@campray.com>
2014-12-09 16:03:28 +0800

    Add industry interface

---
+++ Libraries/Rms.Services/Common/IIndustryService.cs
@@ -0,0 +1,29 @@
+﻿using Rms.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rms.Services.Common
+{ 
+    public partial interface IIndustryService
+    {
+
+        void DeleteIndustry(Industry industry);
+
+
+        Industry GetIndustryById(int industryId);
+
+
+        IList<Industry> GetAllIndustries();
+
+
+        void InsertIndustry(Industry industry);
+
+
+        void UpdateIndustry(Industry industry);
+
+ 
+    }
+}
--- Libraries/Rms.Services/Common/IndustryService.cs
+++ Libraries/Rms.Services/Common/IndustryService.cs
@@ -12,40 +12,40 @@ using Rms.Services.Events;
 
 namespace Rms.Services.Common
 {
-     public partial class IndustryService
+    public partial class IndustryService : IIndustryService
     {
-        #region Constants 
-        #endregion 
-        #region Fields 
-        private readonly IRepository<Industry> _industryRepository;  
-        private readonly ILogger _logger; 
+        #region Constants
+        #endregion
+        #region Fields
+        private readonly IRepository<Industry> _industryRepository;
+        private readonly ILogger _logger;
         private readonly IGenericAttributeService _genericAttributeService;
-        private readonly ILocalizationService _localizationService; 
+        private readonly ILocalizationService _localizationService;
         private readonly ICacheManager _cacheManager;
         private readonly IEventPublisher _eventPublisher;
         #endregion
 
         #region Ctor
 
-        public IndustryService(IRepository<Industry> industryRepository, 
-            ILogger logger, 
+        public IndustryService(IRepository<Industry> industryRepository,
+            ILogger logger,
             IGenericAttributeService genericAttributeService,
             ILocalizationService localizationService,
             IEventPublisher eventPublisher,
             ICacheManager cacheManager)
         {
-            this._industryRepository = industryRepository; 
-            this._logger = logger; 
+            this._industryRepository = industryRepository;
+            this._logger = logger;
             this._genericAttributeService = genericAttributeService;
             this._localizationService = localizationService;
-            this._cacheManager = cacheManager; 
+            this._cacheManager = cacheManager;
             this._eventPublisher = eventPublisher;
         }
 
         #endregion
-        
+
         #region Methods
- 
+
 
         #region Delivery dates
 
@@ -75,10 +75,10 @@ namespace Rms.Services.Common
             return _industryRepository.GetById(industryId);
         }
 
-       /// <summary>
-       /// 
-       /// </summary>
-       /// <returns></returns>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public virtual IList<Industry> GetAllIndustries()
         {
             var query = from dd in _industryRepository.Table
@@ -120,7 +120,7 @@ namespace Rms.Services.Common
 
         #endregion
 
-      
+
 
         #endregion
     }
