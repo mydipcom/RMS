1f08bf8663271233d0f11b80d408e9f5661554ec
andy.lin <andy.lin@campray.com>
2014-12-09 16:26:48 +0800

    Add Mapping for industry

---
+++ Libraries/Rms.Data/Mapping/Common/IndustryMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rms.Core.Domain.Common;
+
+namespace Rms.Data.Mapping.Common
+{
+    
+    public partial class IndustryMap : EntityTypeConfiguration<Industry>
+    {
+        public IndustryMap()
+        {
+            this.ToTable("Industry");
+            this.HasKey(a => a.Id); 
+        }
+    }
+}
