781562b803073b6dfffbe085d0228ba4a8f2f32d
alvisyu-campray <alvis.yu@campray.com>
2014-12-15 14:20:31 +0800

    update

---
+++ Presentation/Rms.Web/Models/Customer/CustomerProfileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rms.Web.Models.Customer
+{
+    public class CustomerProfileModel
+    {
+        public string Email { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
