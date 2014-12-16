4588e49a5ba1b56ff9a21054eda86d8f71a3c7b8
alvisyu-campray <alvis.yu@campray.com>
2014-12-16 14:39:59 +0800

    update

--- Presentation/Rms.Web.Framework/Mvc/NopMetadataProvider.cs
+++ Presentation/Rms.Web.Framework/Mvc/NopMetadataProvider.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Mvc;
 using Rms.Core;
+using Rms.Core.Infrastructure;
+using Rms.Services.Localization;
 
 namespace Rms.Web.Framework.Mvc
 {
@@ -13,7 +16,8 @@ namespace Rms.Web.Framework.Mvc
     /// </summary>
     public class NopMetadataProvider : DataAnnotationsModelMetadataProvider
     {
-        protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType, Func<object> modelAccessor, Type modelType, string propertyName)
+        protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType,
+            Func<object> modelAccessor, Type modelType, string propertyName)
         {
             var metadata = base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
             var additionalValues = attributes.OfType<IModelAttribute>().ToList();
@@ -26,5 +30,29 @@ namespace Rms.Web.Framework.Mvc
             }
             return metadata;
         }
+    }
+
+
+
+    /// <summary>
+    /// Nop Model Validator MetadataProvider
+    /// </summary>
+    public class NopModelValidatorMetadataProvider : DataAnnotationsModelValidatorProvider
+    {
+        protected override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, ControllerContext context,
+            IEnumerable<Attribute> attributes)
+        {
+            var localizationService = EngineContext.Current.Resolve<ILocalizationService>();
+            foreach (var attribute in attributes)
+            {
+                if (attribute is ValidationAttribute)
+                {
+                    var required = attribute as ValidationAttribute;
+                    required.ErrorMessage = localizationService.GetResource(required.ErrorMessage);
+                }
+            }
+            return base.GetValidators(metadata, context, attributes);
+        }
+
     }
 }
